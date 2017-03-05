ed50b88a89599c7bd190a3f88d1e98688daf53fb
Ana Cañizares <ana2588@gmail.com>
2017-03-05 21:55:00 +0100

    Generate functions with a fix but arbitrary number of arguments

--- utils.py
+++ utils.py
@@ -1,20 +1,44 @@
 # coding: utf-8
 
 from functools import reduce
+from decorator import FunctionMaker
+from inspect import getargspec
+
+def fnop(op, *fs):
+    """ Combine an arbitrary number of functions with op
+        in a new function.
+
+        Arguments:
+            op: operator name as a string"""
+    n = len(getargspec(fs[0]).args)
+    correct = True
+    for f in fs[1:]:
+        correct = correct and n == len(getargspec(f).args)
+    if not correct:
+        raise ValueError('All functions must have the same arity.')
+    arg_names = ['x'+str(i) for i in range(n)]
+    args = '(' + ', '.join(arg_names) + ')'# '(x0, x1, ..., x(n-1))'
+    body = str('return reduce(lambda x,y: x ' + op + ' y, [f' + args + ' for f in fs])')
+    res = FunctionMaker.create('res' + args, body, {'fs': fs, 'reduce': reduce}, addsource=True)
+    return res
 
 def fnand(*fs):
-    def res(*args):
-        return reduce(lambda x,y: x and y, [f(*args) for f in fs])
-    return res
+    """ Combine an arbitrary number of functions with `and`
+        in a new function"""
+    return fnop('and', *fs)
 
 def fnor(*fs):
-    def res(*args):
-        return reduce(lambda x,y: x or y, [f(*args) for f in fs])
-    return res
+    """ Combine an arbitrary number of functions with `or`
+        in a new function."""
+    return fnop('or', *fs)
 
 def fnnot(f):
-    def res(*args):
-        return not f(*args)
+    """ Negation of a function."""
+    n = len(getargspec(f).args)
+    arg_names = ['x'+str(i) for i in range(n)]
+    args = '(' + ', '.join(arg_names) + ')'# '(x0, x1, ..., x(n-1))'
+    doc = "Returns the negation of %s%s" % (f.__name__, args)
+    res = FunctionMaker.create('res' + args, 'return not f' + args, {'f':f}, addsource=True, doc=doc)
     return res
 
 def one_arg(f):
